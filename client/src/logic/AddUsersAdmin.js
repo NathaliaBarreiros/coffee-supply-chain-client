478c7d8bc82849f4cefe7066c9cb5c4d79e562ea
Nath <nathalia.barreirosf@gmail.com>
2022-05-10 19:16:16 -0500

    ipfs added

--- client/src/logic/AddUsersAdmin.js
+++ client/src/logic/AddUsersAdmin.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { create, CID } from "ipfs-http-client";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { Grid, Container, Typography, Button } from "@mui/material";
@@ -10,6 +11,8 @@ import role from "../data/roles.json";
 import { ethers } from "ethers";
 import supplychainUserABI from "../contracts/SupplyChainUser.json";
 
+import PreviewImage from "../components/PreviewImage";
+
 const CoffeeSupplyChainAddress = "0xa108A7C2e0417aF523eadFA4Cf628126BEFB0534";
 const SupplyChainUserAddress = "0x8c3ADb90d52223eAf8C5BeD5a6D44da08d4b0BaE";
 
@@ -19,8 +22,11 @@ const INITIAL_FORM_USER_STATE = {
 	email: "",
 	role: "",
 	isActive: false,
-	profileHash: "",
+	// profileHash: "",
+	profileHash: null,
 };
+
+const SUPPORTED_FORMATS = ["image/jpg", "image/png", "image/jpeg", "image/gif"];
 
 const FORM_USER_VALIDATION = Yup.object().shape({
 	userAddress: Yup.string()
@@ -32,7 +38,22 @@ const FORM_USER_VALIDATION = Yup.object().shape({
 	email: Yup.string().email("Email inválido").required("Requerido"),
 	role: Yup.string().required("Requerido"),
 	isActive: Yup.boolean().required("requerido"),
-	profileHash: Yup.string(),
+	// profileHash: Yup.string(),
+	profileHash: Yup.mixed()
+		.required("requerido")
+		.nullable()
+		.test(
+			"size",
+			"File size is too big",
+			(value) => value && value.size <= 1024 * 1024 // 5MB
+		)
+		.test(
+			"type",
+			"Invalid file format selection",
+			(value) =>
+				// console.log(value);
+				!value || (value && SUPPORTED_FORMATS.includes(value?.type))
+		),
 });
 
 const AddUsersAdmin = () => {
@@ -45,6 +66,22 @@ const AddUsersAdmin = () => {
 		profileHash: "-",
 	});
 	const [userRegistered, setUserRegistered] = useState([]);
+	const [images, setImages] = useState([]);
+	let ipfs;
+	try {
+		ipfs = create({
+			url: "https://ipfs.infura.io:5001/api/v0",
+			// headers: {
+			// 	authorization,
+			// },
+		});
+		//console.log(authorization);
+		//console.log("CONECTED");
+		//console.log("ipfs", ipfs);
+	} catch (error) {
+		console.error("IPFS error ", error);
+		ipfs = undefined;
+	}
 
 	useEffect(() => {
 		// listener del evento "UserUpdate", cuando ya se registra en la blockchain
@@ -75,6 +112,11 @@ const AddUsersAdmin = () => {
 		};
 	}, []);
 
+	// const getUrl = async () => {
+	// 	const path = images.map((image) => image.path);
+	// 	return path;
+	// };
+
 	const handleSubmit = async (values) => {
 		//console.log(values);
 		console.log(values["userAddress"]);
@@ -83,6 +125,42 @@ const AddUsersAdmin = () => {
 		console.log(values["email"]);
 		console.log(values["isActive"]);
 		console.log(values["profileHash"]);
+
+		if (!values["profileHash"] || values["profileHash"].length === 0) {
+			console.log("No files selected!");
+		}
+
+		const file = values["profileHash"];
+		const result = await ipfs.add(file);
+
+		const url = `https://ipfs.infura.io/ipfs/${result.path}`;
+		console.log("url: ", url);
+
+		const uniquePaths = new Set([
+			...images.map((image) => image.path),
+			result.path,
+		]);
+
+		const uniqueImages = [...uniquePaths.values()].map((path) => {
+			return [
+				...images,
+				{
+					cid: result.cid,
+					path: result.path,
+				},
+			].find((image) => image.path === path);
+		});
+
+		// setImages([
+		// 	...images,
+		// 	{
+		// 		cid: result.cid,
+		// 		path: result.path,
+		// 	},
+		// ]);
+		setImages(uniqueImages);
+
+		console.log("images: ", images);
 
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		//await provider.send("eth_requestAccounts", []);
@@ -98,89 +176,138 @@ const AddUsersAdmin = () => {
 			email: values["email"],
 			role: values["role"],
 			isActive: values["isActive"],
-			profileHash: values["profileHash"],
+			// profileHash: values["profileHash"],
+			profileHash: url,
 		});
+		console.log("userinfo: ", userInfo);
 		await erc20.updateUserForAdmin(
 			values["userAddress"],
 			values["name"],
 			values["email"],
 			values["role"],
 			values["isActive"],
-			values["profileHash"]
+			// values["profileHash"]
+			url
 		);
 	};
 
 	return (
 		<Grid container>
 			<Grid item xs={12}>
-				<Container maxWidth="md">
-					<div>
-						<Formik
-							initialValues={{
-								...INITIAL_FORM_USER_STATE,
-							}}
-							validationSchema={FORM_USER_VALIDATION}
-							onSubmit={(values) => {
-								handleSubmit(values);
-							}}
-						>
-							{({ dirty, isValid }) => {
-								return (
-									<Form>
-										<Grid container spacing={2}>
-											<Grid item xs={12}>
-												<Typography>Add User Details</Typography>
-											</Grid>
-											<Grid item xs={6}>
-												<TextfieldWrapper
-													name="userAddress"
-													label="User Address"
-												/>
-											</Grid>
-											<Grid item xs={6}>
-												<TextfieldWrapper name="name" label="Name" />
-											</Grid>
-											<Grid item xs={6}>
-												<TextfieldWrapper name="email" label="Email" />
-											</Grid>
-											<Grid item xs={6}>
-												<SelectWrapper
-													name="role"
-													label="Role"
-													options={role}
-												/>
-											</Grid>
-											<Grid item xs={6}>
-												<TextfieldWrapper
+				{/* <Container maxWidth="md"> */}
+				<div>
+					<Formik
+						initialValues={{
+							...INITIAL_FORM_USER_STATE,
+						}}
+						validationSchema={FORM_USER_VALIDATION}
+						onSubmit={(values) => {
+							handleSubmit(values);
+						}}
+					>
+						{({
+							dirty,
+							isValid,
+							setTouched,
+							setFieldValue,
+							touched,
+							errors,
+							values,
+						}) => {
+							return (
+								<Form>
+									<Grid container spacing={2}>
+										<Grid item xs={12}>
+											<Typography>Add User Details</Typography>
+										</Grid>
+										<Grid item xs={6}>
+											<TextfieldWrapper
+												name="userAddress"
+												label="User Address"
+											/>
+										</Grid>
+										<Grid item xs={6}>
+											<TextfieldWrapper name="name" label="Name" />
+										</Grid>
+										<Grid item xs={6}>
+											<TextfieldWrapper name="email" label="Email" />
+										</Grid>
+										<Grid item xs={6}>
+											<SelectWrapper name="role" label="Role" options={role} />
+										</Grid>
+										<Grid item xs={6}>
+											{/* <TextfieldWrapper
 													name="profileHash"
 													label="Profile Hash"
+												/> */}
+
+											<label>
+												Profile Hash:{" "}
+												<div>
+													<input
+														name="profileHash"
+														// label="Profile Hash"
+														type="file"
+														onChange={(event) => {
+															setTouched({
+																...touched,
+																profileHash: true,
+															});
+															setFieldValue(
+																"profileHash",
+																event.target.files[0]
+															);
+														}}
+													></input>
+													{touched.profileHash && errors.profileHash ? (
+														<small>{errors.profileHash}</small>
+													) : null}
+												</div>
+											</label>
+											{values.profileHash ? (
+												<PreviewImage
+													className={{ margin: "auto" }}
+													width={100}
+													height={100}
+													file={values.profileHash}
 												/>
-											</Grid>
-											<Grid item xs={6}>
-												<CheckboxWrapper
-													name="isActive"
-													legend="Activity"
-													label="Active User"
-												/>
-											</Grid>
-											<Grid item xs={12}>
-												<Button
-													fullWidth
-													variant="contained"
-													disabled={!dirty || !isValid}
-													type="submit"
-												>
-													{" "}
-													SUBMIT
-												</Button>
-											</Grid>
+											) : null}
+											<div>
+												{images.map((image, index) => (
+													<img
+														alt={`Uploaded #${index + 1}`}
+														src={"https://ipfs.infura.io/ipfs/" + image.path}
+														style={{ maxWidth: "200px", margin: "14px" }}
+														key={image.cid.toString() + index}
+													/>
+												))}
+											</div>
 										</Grid>
-									</Form>
-								);
-							}}
-						</Formik>
-					</div>
-				</Container>
+										<Grid item xs={6}>
+											<CheckboxWrapper
+												name="isActive"
+												legend="Activity"
+												label="Active User"
+											/>
+										</Grid>
+										<Grid item xs={12}>
+											<Button
+												fullWidth
+												variant="contained"
+												disabled={!dirty || !isValid}
+												type="submit"
+											>
+												{" "}
+												SUBMIT
+											</Button>
+										</Grid>
+									</Grid>
+								</Form>
+							);
+						}}
+					</Formik>
+				</div>
+				{/* </Container> */}
 			</Grid>
 		</Grid>
 	);
