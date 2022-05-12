d85a0c8d193101a4e3664ccb34ad2314e93dbb65
Nath <nathalia.barreirosf@gmail.com>
2022-05-12 02:48:27 -0500

    ipfs for add user - logic folder

---
+++ client/src/logic/ipfs/user/ConnectIPFS.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { create } from 'ipfs-http-client';
+
+const ConnectIPFS = () => {
+  let ipfs;
+  try {
+    ipfs = create({
+      url: 'https://ipfs.infura.io:5001/api/v0',
+    });
+    return { conection: ipfs, error: null };
+  } catch (err) {
+    ipfs = undefined;
+    return { conection: null, error: err };
+  }
+};
+
+export default ConnectIPFS;
