b36b7eac29ccc724cace375559deb0a7510d74d8
Nath <nathalia.barreirosf@gmail.com>
2022-05-17 14:37:50 -0500

    uuidv4 integrated in AddFarmDetails function

--- client/src/logic/AddFarmDetails/HandleSubmit.js
+++ client/src/logic/AddFarmDetails/HandleSubmit.js
@@ -1,10 +1,12 @@
-import { getCoffeWriterERC20 } from '../erc20'
+import { v4 as uuid } from 'uuid';
+import { getCoffeWriterERC20 } from '../erc20';
 
 const HandleSubmit = async (values) => {
-  
+  const uniqueID = uuid();
   const erc20 = await getCoffeWriterERC20();
   await erc20.addFarmDetails(
-    values.registrationNo,
+    // values.registrationNo,
+    uniqueID,
     values.farmName,
     values.latitude,
     values.longitude,
